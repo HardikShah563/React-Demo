debcd4bec1a7bf11f7dbfc6adbf3e27b4fcceebf
HardikShah563 <hardikts@gmail.com>
2023-02-14 23:35:58 +0530

    i don't know what changes I've made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 import router from './config/router';
 
 export default function App() {

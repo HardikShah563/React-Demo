ada6192fe40224c79cbabc84611eb3d75e1ca7d8
HardikShah563 <hardikts@gmail.com>
2023-02-15 02:20:26 +0530

    added PrintName file in components

---
+++ src/components/PrintName.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PrintName(props) {
+    return (
+        <>
+            Hello {props.name}
+        </>
+    );
+}
+
--- src/config/router.js
+++ src/config/router.js
@@ -4,14 +4,14 @@ import About from '../screens/About';
 
 const router = createBrowserRouter([
     {
-        path: "/",
+        path: '/',
         element: <Home />,
     }, {
-        path: "/home",
-        element: <Home />
+        path: '/home',
+        element: <Home />,
     }, {
-        path: "/about",
-        element: <About />,
+        path: '/about',
+        element: <About />
     }
 ])
 
